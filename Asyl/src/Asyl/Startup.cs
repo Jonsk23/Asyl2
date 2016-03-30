6eed2169b03ee661b25b05410952cc46dff2f6c6
Jon Skärlina <pinkrope@hotmail.com>
2016-03-30 09:37:12 +0200

    Startup grovt gjord och dependencies tillagda

--- Asyl/src/Asyl/Startup.cs
+++ Asyl/src/Asyl/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
 
 namespace Asyl
 {
@@ -15,17 +17,41 @@ namespace Asyl
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            //var connString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=CatShelter;Integrated Security=True;Pooling=False";
+            //var identityConnString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=IdentityDB;Integrated Security=True;Pooling=False";
+
+            //services.AddEntityFramework()
+            //    .AddSqlServer()
+            //    .AddDbContext<IdentityDbContext>(o => o.UseSqlServer(identityConnString));
+
+            //services.AddIdentity<IdentityUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<IdentityDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            //services.AddEntityFramework()
+            //    .AddSqlServer()
+            //    .AddDbContext<CatShelterContext>(o => o.UseSqlServer(connString));
+
+            ////services.AddTransient<ICatsRepository, DbCatsRepository>();
+            //services.AddTransient<ICatsRepository, TestCatsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
+            app.UseCookieAuthentication(o =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                o.AutomaticChallenge = true;
+                //o.LoginPath = new PathString("/Members/Login/");
             });
+
+            app.UseIdentity();
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application.
