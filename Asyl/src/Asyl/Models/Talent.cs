a97261e20875279b78f9a24f80bdc83e832d9f20
Jon Skärlina <pinkrope@hotmail.com>
2016-04-01 10:23:46 +0200

    nu då

---
+++ Asyl/src/Asyl/Models/Talent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asyl.Models
+{
+    public class Talent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int YearsInPrimarySchool { get; set; }
+        public int YearsInSecondarySchool { get; set; }
+        public string WorkExperience { get; set; }
+        public bool SpeaksSwedish { get; set; }
+        public bool SpeaksEnglish { get; set; }
+        public bool DrivingLicense { get; set; }
+
+
+        //Navigation property
+        public virtual ICollection<Application> Applications { get; set; }
+
+        //wtf talents! -----
+    }
+}
