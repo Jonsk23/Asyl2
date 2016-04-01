ec7836cb43b5cd021fe028ee5c476754983e7fe6
Joachim Good <joachim.good@hotmail.com>
2016-04-01 11:03:06 +0200

    Lagt in en account controller

---
+++ Asyl/src/Asyl/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Asyl.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        SignInManager<IdentityUser> signInManager;
+        UserManager<IdentityUser> userManager;
+        IdentityDbContext idContext;
+
+
+        //En konstruktor som tar in kontexterna
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IdentityDbContext idContext)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.idContext = idContext;
+
+            
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
