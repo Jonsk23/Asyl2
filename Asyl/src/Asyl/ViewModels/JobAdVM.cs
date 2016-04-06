d1ec8fa59d807a504a5483b7304ea35ae042af1f
martin nathorst-westfelt <westfelt@gmail.com>
2016-04-06 12:00:38 +0200

    createjobad klar

--- Asyl/src/Asyl/ViewModels/JobAdVM.cs
+++ Asyl/src/Asyl/ViewModels/JobAdVM.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Asyl.ViewModels
 {
     public class JobAdVM
     {
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string FieldOfWork { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
+        [Range(1,40)]
         public int LocationId { get; set; }
+        [Required]
+        [Range(1,104)]
         public int DurationInWeeks { get; set; }
     }
 }
